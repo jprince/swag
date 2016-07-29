c91b771041db875a6e61989d4617cec653c630d1
Josh Prince <josh.prince@arcadiasolutions.com>
2016-07-28 23:41:39 -0400

    Render the list of tickets

--- app/tickets/ticket-list.component.js
+++ app/tickets/ticket-list.component.js
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
-import { TicketComponent } from './ticket.component';
 import { TicketService } from './ticket.service';
 
 @Component({
-  directives: [TicketComponent],
   selector: 'ticket-list',
   templateUrl: './ticket-list.component.html'
 })
--- app/tickets/ticket.component.js
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ticket',
-  templateUrl: './ticket.component.html'
-})
-
-export class TicketComponent {}
