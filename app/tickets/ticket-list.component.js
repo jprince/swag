7b16190f3987f11d2d2c7e28ee67613d05fac020
Josh Prince <josh.prince@arcadiasolutions.com>
2016-07-28 20:58:00 -0400

    Add a simple ticket component

--- app/tickets/ticket-list.component.js
+++ app/tickets/ticket-list.component.js
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { TicketComponent } from './ticket.component';
 
 @Component({
+  directives: [TicketComponent],
   selector: 'ticket-list',
   templateUrl: './ticket-list.component.html'
 })
---
+++ app/tickets/ticket.component.js
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ticket',
+  templateUrl: './ticket.component.html'
+})
+
+export class TicketComponent {}
