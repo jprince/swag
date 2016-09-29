3312bd866730b84355a1ee95cbe6863b8c7ca898
Josh Prince <josh.prince@arcadiasolutions.com>
2016-09-29 17:07:18 -0400

    Cleanup pipe logic

--- client/app/tickets/for-story-point.pipe.js
+++ client/app/tickets/for-story-point.pipe.js
@@ -6,8 +6,11 @@ import {Pipe, PipeTransform} from '@angular/core'
 
 export class ForStoryPointPipe implements PipeTransform {
   transform(value, storyPoints) {
-    if (typeof value === "undefined" || value === null) {
+    if (typeof value === 'undefined' || value === null) {
       return;
+    }
+    if ((typeof storyPoints === 'undefined' || storyPoints === null) || (storyPoints === '')) {
+      return value;
     }
     return value.filter((item) => parseInt(item.fields.customfield_10002) === parseInt(storyPoints));
   }
